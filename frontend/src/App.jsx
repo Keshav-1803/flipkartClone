14a497506d5548005746e50d3f526fdbd2611883
ChetanSingh14 <siradhanachetan14@gmail.com>
2024-10-18 16:23:16 +0530

    signin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Category from './components/Categories/Category'
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import User from './pages/SIgnUp/SignUp'
 
 export default function App() {
   return (
     <div>
        <Home/>
+       <Login/>
+       <User/>
     </div>
   )
 }
